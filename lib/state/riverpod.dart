f981fc4ca9b2d58e0bc203d0a9a895113acf3610
sinjikoro <shinnaga.dev@gmail.com>
2022-12-10 01:31:19 +0900

    a

--- lib/state/riverpod.dart
+++ lib/state/riverpod.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class Riverpod extends StatelessWidget {
   const Riverpod({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Riverpod')),
+      body: const Text('りばぽ'),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          GoRouter.of(context).pop();
+        },
+        child: const Icon(Icons.abc),
+      ),
+    );
   }
 }
--- lib/state/state.dart
+++ lib/state/state.dart
@@ -1,10 +1,27 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class State extends StatelessWidget {
   const State({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Text('状態管理');
+    return Scaffold(
+      appBar: AppBar(title: const Text('状態管理')),
+      body: const Text('状態管理'),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.abc), label: 'test'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.abc_outlined), label: 'test'),
+          BottomNavigationBarItem(icon: Icon(Icons.abc_rounded), label: 'test'),
+        ],
+        onTap: (index) {
+          GoRouter.of(context).push('/riverpod');
+        },
+      ),
+    );
   }
 }
